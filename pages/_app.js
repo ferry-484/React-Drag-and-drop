df264f311aadf8156d11034f53d96c7d478d28c8
ferry-484 <80960981+ferry-484@users.noreply.github.com>
2021-05-18 20:35:05 +0530

    Add files via upload

--- pages/_app.js
+++ pages/_app.js
@@ -1,13 +1,53 @@
+// import { ApolloProvider } from "@apollo/client";
+// import client from "../apollo-client";
+// import '../styles/app.css';
+// <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+  
+// </link>
+// export default function App({ Component, pageProps }) {
+//     return (
+//       <ApolloProvider client={client}>
+//         <Component {...pageProps} />
+//       </ApolloProvider>
+//     );
+//   }
+
 import { ApolloProvider } from "@apollo/client";
 import client from "../apollo-client";
-import '../styles/app.css';
-<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
-  
-</link>
+import React, { useEffect } from "react";
+import "../styles/app.css";
+<link
+  rel="stylesheet"
+  href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css"
+  integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh"
+  crossorigin="anonymous"
+></link>;
+
+
 export default function App({ Component, pageProps }) {
-    return (
-      <ApolloProvider client={client}>
-        <Component {...pageProps} />
-      </ApolloProvider>
-    );
-  }
+  useEffect(() => {
+    if ("serviceWorker" in navigator) {
+      window.addEventListener("load", function () {
+        navigator.serviceWorker.register("/sw.js").then(
+          function (registration) {
+            console.log(
+              "Service Worker registration successful with scope: ",
+              registration.scope
+            );
+          },
+
+          function (err) {
+            console.log("Service Worker registration failed: ", err);
+          }
+        );
+      });
+    }
+  }, []);
+
+
+  return (
+    <ApolloProvider client={client}>
+      <Component {...pageProps} />
+    </ApolloProvider>
+  );
+}
